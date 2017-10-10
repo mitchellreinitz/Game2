4766afd8abfc09fd67810ae22765475301e82e24
Mitchell Reinitz <mitchellreinitz1@mitchells-macbook-pro.local>
2017-10-09 20:51:56 -0400

    Initial Commit

---
+++ Game2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game2
+//
+//  Created by Mitchell Reinitz on 10/9/17.
+//  Copyright © 2017 Mitchell Reinitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
